81b146d35bedd26cc9abf1caac79a57bca0f5767
BleueBear <flashbob@gmail.com>
2024-01-11 12:56:10 +0000

    update
    added prop protection pp_propprotection 1 = enabled, disabled by default.

--- lua/coding/sh_xtras.lua
+++ lua/coding/sh_xtras.lua
@@ -1,3 +1,4 @@
+CreateConVar("pp_propprotection", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_NOTIFY})
 function GetFallDamage(ply, speed)
     if ply:IsPlayer() and ply.Holding then return 0 end
     if GetConVar("mp_falldamage"):GetFloat() == 1 then return math.floor(speed) / 2 end
@@ -7,6 +8,7 @@ end
 hook.Add("GetFallDamage", "Allow Player No Damage", GetFallDamage)
 local function PlayerPickup(ply, ent)
     if ent.Holding == nil then ent.Holding = false end
+    if ent.IsOwner and GetConVar("pp_propprotection") == 1 then return true end
     if ply:IsAdmin() and ent:GetClass():lower() == "player" then
         ent:SetLocalVelocity(ent:GetPos())
         ent.Holding = true
@@ -21,3 +23,10 @@ local function PlayerDrop(ply, ent)
 end
 
 hook.Add("PhysgunDrop", "Allow Player Drop", PlayerDrop)
+hook.Add("PlayerSpawnedEffect", "PropProtection", function(ply, model, ent) ent.IsOwner = true end)
+hook.Add("PlayerSpawnedNPC", "PropProtection", function(ply, ent) ent.IsOwner = true end)
+hook.Add("PlayerSpawnedProp", "PropProtection", function(ply, model, ent) ent.IsOwner = true end)
+hook.Add("PlayerSpawnedRagdoll", "PropProtection", function(ply, model, ent) ent.IsOwner = true end)
+hook.Add("PlayerSpawnedSENT", "PropProtection", function(ply, ent) ent.IsOwner = true end)
+hook.Add("PlayerSpawnedSWEP", "PropProtection", function(ply, ent) ent.IsOwner = true end)
+hook.Add("PlayerSpawnedVehicle", "PropProtection", function(ply, ent) ent.IsOwner = true end)
