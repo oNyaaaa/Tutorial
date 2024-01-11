1d7e138f5f251395d5a79bce3d313c1fefdc44b3
BleueBear <flashbob@gmail.com>
2024-01-11 11:00:41 +0000

    Update
    Added Coding.Config.FindPlayer -- Arguements (name) returns Entity
    Added Console command so you can find the lua code eaiser:
    Console command: nyaaa_info
    Coding.Config.IgnitePlayer -- Arguements (targ,length,radius)
    e.g lua_run Coding.Config.IgnitePlayer("Nya",10,10)
    Coding.Config.KickPlayer -- Arguements (targ,reason)
    Added -- Allow admins to pick up players with no fall damage and smooth movement
    Added -- Fall damage is realism now -- mp_falldamage 0 = 0 fall damage / 1 = realism falll damage

--- lua/coding/sh_notes.lua
+++ lua/coding/sh_notes.lua
@@ -65,4 +65,37 @@ HUD:
 local circ = Coding.Config.CircleNew(CIRCLE_OUTLINED, true, color_white, 50, 240, 140)
 hook.Add("HUDPaint", "LIL", function()  Coding.Config.HudCircleFill(LocalPlayer(), 0000444, circ, 0.2, function() print("You win 100") end) end)
 hook.Add("HUDPaint", "BigText", function() Coding.Config.EazyText("hello", 10, 10, "Big", Color(255, 0, 0)) end)
+
+Player: 
+Added FindPlayer -- Arguements (name) returns Entity
 ]]
+
+local function info()
+
+    print[[
+        Example Code Below:
+    Coding.Config.DFrame -- Same as Dframe with arguements (title, Pos =x , Pos = y, Width = w , Height = h)
+    Coding.Config.DPanelMain(panel) -- Same as DPanel with arguements of (Panel)
+    Coding.Config.DPanelPatch -- Same as DPanel with arguements(panel,x,y,w,h)
+    Coding.Config.SheetAdd -- Creates a sheet (panel)
+    Coding.Config.Sheet -- Creates a panel for the sheet(sheet, img)
+    Coding.Config.DButton -- Creates a dbutton with arguements (panelx, x, y, w, h, text, clock_func) returns DButton
+    Coding.Config.CheckBoxLabel-- Creates a CheckBox with a label with arguements (panel, x, y, text, convar, val, CheckTouch)
+    Coding.Config.DListView -- Creates a DListView with arguements (panel)
+    Coding.Config.DListViewAddColumn -- Creates DListViewAddColumn dbutton with arguements (panel, name)
+    Coding.Config.DListViewAddLine -- Creates a DListViewAddLine with arguements (panel, ...)
+    Coding.Config.DListViewOnRow -- Creates a DListViewOnRow with arguements (panel, func)
+    Coding.Config.MenuIconAdd -- Creates a MenuIconAdd with arguements (panel, name, img, x, y, CurrFunc, Table)
+    
+    HUD:
+    Coding.Config.CircleNew -- Creates a circle function with arguements (type, mat, col, radius, x, y) returns circle
+    Coding.Config.HudCircleFill -- Creates a circle with arguements draw in a HUDPaint (self, id, circle, slowness, complete) returns percent
+    Coding.Config.EazyText -- Creates a Easy Text with arguements draw in a HUDPaint (text, x, y, fnt, col)
+    Coding.Config.AnimatedText -- Creates Animated Text with arguements draw in a HUDPaint (id, text, x, y, fnt, col, times)
+    
+    Player:
+    Added FindPlayer -- Arguements (name) returns Entity
+    ]]
+
+end
+concommand.Add("nyaaa_info", info)
--- lua/coding/sh_player.lua
+++ lua/coding/sh_player.lua
@@ -1,4 +1,4 @@
-function FindPlayer(name)
+function Coding.Config.FindPlayer(name)
     local count = 0
     local names = NULL
     for k, v in pairs(player.GetAll()) do
@@ -17,3 +17,39 @@ function FindPlayer(name)
     end
     return NULL
 end
+
+function Coding.Config.IgnitePlayer(targ, num, num2)
+    local target = Coding.Config.FindPlayer(targ)
+    if target == NULL then return end
+    target:Ignite(num, num2)
+end
+
+function Coding.Config.KickPlayer(targ, reason)
+    local target = Coding.Config.FindPlayer(targ)
+    if target == NULL then return end
+    target:Kick(reason)
+end
+
+function GetFallDamage(ply, speed)
+    if ply:IsPlayer() and ply.Holding then return 0 end
+    if GetConVar("mp_falldamage"):GetFloat() == 1 then return math.floor(speed) / 2 end
+    return 0
+end
+
+hook.Add("GetFallDamage", "Allow Player No Damage", GetFallDamage)
+local function PlayerPickup(ply, ent)
+    if ent.Holding == nil then ent.Holding = false end
+    if ply:IsAdmin() and ent:GetClass():lower() == "player" then
+        ent:SetLocalVelocity(ent:GetPos())
+        ent.Holding = true
+        return true
+    end
+end
+
+hook.Add("PhysgunPickup", "Allow Player Pickup", PlayerPickup)
+local function PlayerDrop(ply, ent)
+    if ent.Holding == nil then ent.Holding = false end
+    ent.Holding = false
+end
+
+hook.Add("PhysgunDrop", "Allow Player Drop", PlayerDrop)
